445c799bcc3c650e467fbfdb91bf5f05de347d60
Luis Ruediger <luisruediger27@gmail.com>
2022-03-04 14:56:21 -0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,52 @@
+//iniciar variáveis
+
+let board = ["", "", "", "", "", "", "", "", ""];
+let playerTime = 0;
+let symbol = ["o", "x"];
+let gameOver = false;
+
+let winStates = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function handleMove(position) {
+  if (gameOver) {
+    return;
+  }
+
+  if (board[position] == "") {
+    board[position] = symbol[playerTime];
+
+    gameOver = isWin();
+
+    if (gameOver == false) {
+      playerTime = playerTime == 0 ? 1 : 0;
+    }
+  }
+  return gameOver;
+}
+
+function isWin() {
+  for (let i = 0; i < winStates.length; i++) {
+    let seq = winStates[i];
+    let position1 = seq[0];
+    let position2 = seq[1];
+    let position3 = seq[2];
+
+    if (
+      board[position1] == board[position2] &&
+      board[position1] == board[position3] &&
+      board[position1] != ""
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
